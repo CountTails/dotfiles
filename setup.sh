e2a5137eef13fb3dca6639d9fddfaac77d21addd
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2021-12-16 12:56:31 -0800

    First version of setup script complete

--- setup.sh
+++ setup.sh
@@ -1,7 +1,124 @@
 #! /bin/bash
+SCRIPT_DIR=$HOME/.dotfiles
 
-# TODO: 
-    # 1) Configure and install homebrew
-    # 2) Set up symlinks in home directory
-    # 3) Install homebrew packages
-    # 4) Change shell to zsh if not already
+function print_welcome_banner {
+    echo "================================================================================="
+    echo "Starting the setup script"
+    echo "================================================================================="
+}
+
+function install_homebrew {
+    echo "================================================================================="
+    echo "Installing the homebrew package manager for $(uname)"
+    echo "================================================================================="
+
+    if [ "$(uname)" == "Darwin" ] then
+         xcode-select --install
+         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else if [ "$(uname)" == "Linux" ] then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else then
+        echo "=========================================================================================="
+        echo "Unrecognized OS detected. Unable to install the homebrew package manager"
+        echo "=========================================================================================="
+        exit(1)
+    fi
+
+    if [ $? -eq 0 ] then
+        echo "=========================================================================================="
+        echo "Successfully installed the homebrew package manager"
+        echo "=========================================================================================="
+    else then
+        echo "=========================================================================================="
+        echo "Failed to install the homebrew package manager"
+        echo "=========================================================================================="
+        exit(1)
+    fi
+}
+
+function set_up_links {
+
+    echo "================================================================================="
+    echo "Setting up symlinks to the dofiles directory in the home directory"
+    echo "================================================================================="
+
+    DOTFILES=(.gitconfig .ssh/ .vimrc .vscode/ .zprofile .zshrc)
+
+    for file in "${DOTFILES[@]}"
+    do
+        if [ -f $HOME/$file ] then
+            rm -f $HOME/$file  
+        else if [ -d $HOME/$file ] then
+            rm -rf $HOME/$file
+        fi
+
+    done
+
+    for file in  "${DOTFILES[@]}"
+    do
+        ln -s $SCRIPT_DIR/$file $HOME/$file
+    done
+
+    echo "================================================================================="
+    echo "Successfully linked dotfiles to the home directory"
+    echo "================================================================================="
+
+}
+
+function install_packages {
+    echo "================================================================================="
+    echo "Installing packages listed in $SCRIPTDIR/Brewfile"
+    echo "================================================================================="
+
+    /bin/bash "$(brew bundle)"
+
+    if [ $? -eq 0 ] then
+        echo "=========================================================================================="
+        echo "All packages successfully installed"
+        echo "=========================================================================================="
+    else then
+        echo "=========================================================================================="
+        echo "Something went wrong installing packages"
+        echo "=========================================================================================="
+    fi
+
+}
+
+function switch_shells {
+    echo "================================================================================="
+    echo "Preparing to switch the default shell to ZSH"
+    echo "================================================================================="
+
+    if [ "$SHELL" == "/bin/zsh" ] then
+        echo "The default shell is already ZSH. Horray!!!"
+    else then
+        chsh "$(which zsh)"
+
+        if [ $? -eq 0 ] then
+            echo "======================================================================================"
+            echo "Default shell successfully changed to ZSH"
+            echo "======================================================================================"
+        else then
+            echo "======================================================================================"
+            echo "The default shell could not be changes to ZSH"
+            echo "======================================================================================"
+        fi
+    fi
+}
+
+function print_exit_banner {
+    echo "================================================================================="
+    echo "Setup script complete. Happy coding!"
+    echo "================================================================================="
+}
+
+function main {
+    print_welcome_banner
+    install_homebrew
+    set_up_links
+    install_packages
+    switch_shells
+    print_exit_banner
+}
+
+main
