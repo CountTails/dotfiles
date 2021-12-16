a3d0fddd9dc823df3ef44d9787ac67cf5f50b76a
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2021-12-15 23:44:56 -0800

    Added outline of setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-echo "Hello World!"
+
+# TODO: 
+    # 1) Configure and install homebrew
+    # 2) Set up symlinks in home directory
+    # 3) Install homebrew packages
+    # 4) Change shell to zsh if not already
