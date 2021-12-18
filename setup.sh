04a55de8511af4d9813b95d9658f5f3376ce8582
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2021-12-17 19:03:35 -0800

    Script adds homebrew to path in zsh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+
+
 SCRIPT_DIR=$HOME/.dotfiles
 
 function print_welcome_banner {
@@ -16,9 +18,11 @@ function install_homebrew {
          xcode-select --install
          /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
          eval "$(/opt/homebrew/bin/brew shellenv)"
+         echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' > $SCRIPT_DIR/.zprofile
     elif [ $(uname) = Linux ] ; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' > $SCRIPT_DIR/.zprofile
     else 
         echo "=========================================================================================="
         echo "Unrecognized OS detected. Unable to install the homebrew package manager"
