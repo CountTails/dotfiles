d4d6c3835c2b281cd5098a8930620080ff69c1b1
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-07-29 22:17:34 -0700

    Update setup script messaging system

--- setup.sh
+++ setup.sh
@@ -3,65 +3,60 @@
 
 SCRIPT_DIR=$HOME/.dotfiles
 
-function print_welcome_banner {
-    echo "================================================================================="
-    echo "Starting the setup script"
-    echo "================================================================================="
+function wrapped_text {
+    msg="=>  $*"
+    edge=$(echo "$msg" | sed 's/./=/g')
+    echo "$edge"
+    echo "$msg"
+    echo "$edge"
 }
 
 function install_homebrew {
-    echo "================================================================================="
-    echo "Installing the homebrew package manager for $(uname)"
-    echo "================================================================================="
+    
+    wrapped_text "Preparing to install the homebrew package manager"
 
     if [ $(uname) = Darwin ] ; then
-         xcode-select --install
-         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-         if [ $(uname -m) = x86_64 ] ; then
-             eval "$(/usr/local/bin/brew shellenv)"
-         elif [ $(uname -m) = arm64 ] ; then
-            eval "$(/opt/homebrew/bin/brew shellenv)"
+         xcode-select -p
+         if [ $? -eq 0 ] ; then
+
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            if [ $(uname -m) = x86_64 ] ; then
+                eval "$(/usr/local/bin/brew shellenv)"
+            elif [ $(uname -m) = arm64 ] ; then
+                eval "$(/opt/homebrew/bin/brew shellenv)"
+            else
+                wrapped_text "Unsupported architecture ($uname -m). Unable to install the homebrew package manager"
+                exit 1
+            fi
          else
-             echo "================================================================================"
-             echo "Unsupported architecture ($uname -m). Unable to install the homebrew package manager"
-             echo "================================================================================"
-             exit 1
+            wrapped_text "xcode command line tools are required to install homebrew.\nRun the following to install them:\n\n\t $ xcode-select --install\nRe-run the script after installation completes"
+           exit 1 
          fi
     elif [ $(uname) = Linux ] ; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         if [ $(uname -m) != x86_64 ] ; then
-             echo "================================================================================"
-             echo "Unsupported architecture ($uname -m). Unable to install the homebrew package manager"
-             echo "================================================================================"
+             wrapped_text "Unsupported architecture ($uname -m). Unable to install the homebrew package manager"
              exit 1
         fi
         eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     else 
-        echo "=========================================================================================="
-        echo "Unrecognized OS detected. Unable to install the homebrew package manager"
-        echo "=========================================================================================="
+        wrapped_text "Unrecognized OS detected. Unable to install the homebrew package manager"
         exit 1
     fi
 
     if [ $? -eq 0 ] ; then
-        echo "=========================================================================================="
-        echo "Successfully installed the homebrew package manager"
-        echo "=========================================================================================="
+        wrapped_text "Successfully installed the homebrew package manager"
     else 
-        echo "=========================================================================================="
-        echo "Failed to install the homebrew package manager"
-        echo "=========================================================================================="
+        wrapped_text "Failed to install the homebrew package manager"
         exit 1
     fi
 }
 
 function set_up_links {
 
-    echo "================================================================================="
-    echo "Setting up symlinks to the dofiles directory in the home directory"
-    echo "================================================================================="
+    wrapped_text "Setting up symlinks to the dofiles directory in the home directory"
 
-    DOTFILES=(.gitconfig .ssh .vimrc .vscode .zprofile .zshrc)
+    DOTFILES=(.gitconfig .ssh .vimrc .zprofile .zshrc)
 
     for file in "${DOTFILES[@]}"
     do
@@ -75,37 +70,28 @@ function set_up_links {
     for file in  "${DOTFILES[@]}"
     do
         ln -s $SCRIPT_DIR/$file $HOME/$file
+        echo "Linked $SCRIPT_DIR/$file to $HOME/$file"
     done
 
-    echo "================================================================================="
-    echo "Successfully linked dotfiles to the home directory"
-    echo "================================================================================="
+    wrapped_text "Successfully linked dotfiles to the home directory"
 
 }
 
 function install_packages {
-    echo "================================================================================="
-    echo "Installing packages listed in $SCRIPT_DIR/Brewfile"
-    echo "================================================================================="
+    wrapped_text "Installing packages listed in $SCRIPT_DIR/Brewfile"
 
-    /bin/bash "$(brew bundle)"
+    /bin/bash "$(brew bundle -v)"
 
     if [ $? -eq 0 ] ; then
-        echo "=========================================================================================="
-        echo "All packages successfully installed Horray!!!"
-        echo "=========================================================================================="
+        wrapped_text "All packages successfully installed Horray!!!"
     else
-        echo "=========================================================================================="
-        echo "Not all packages were successfully installed! Continuing with setup."
-        echo "=========================================================================================="
+        wrapped_text "Not all packages were successfully installed! Continuing with setup."
     fi
 
 }
 
 function switch_shells {
-    echo "================================================================================="
-    echo "Preparing to switch the default shell to ZSH"
-    echo "================================================================================="
+    wrapped_text "Preparing to switch the default shell to ZSH"
 
     if [ "$SHELL" == "/bin/zsh" ] ; then
         echo "The default shell is already ZSH. Horray!!!"
@@ -122,32 +108,22 @@ function switch_shells {
         chsh -s "$(which zsh)"
 
         if [ $? -eq 0 ] ; then
-            echo "======================================================================================"
-            echo "Default shell successfully changed to ZSH"
-            echo "======================================================================================"
+            wrapped_text "Default shell successfully changed to ZSH"
         else 
-            echo "======================================================================================"
-            echo "The default shell could not be changes to ZSH"
-            echo "======================================================================================"
+            wrapped_text "The default shell could not be changes to ZSH"
             exit 1
         fi
     fi
 }
 
-function print_exit_banner {
-    echo "================================================================================="
-    echo "Setup script completed successfully. Happy coding!"
-    echo "================================================================================="
-    exit 0
-}
 
 function main {
-    print_welcome_banner
+    wrapped_text "Starting the setup script"
     install_homebrew
     set_up_links
     install_packages
     switch_shells
-    print_exit_banner
+    wrapped_text "Setup script completed successfully. Happy coding!"
 }
 
 main
