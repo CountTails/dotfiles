b7c80401570aec1c1da2b841312146f3bbce6273
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2022-09-16 22:33:53 -0700

    New file for only aliases; added some common ls aliases and python2 -> python3

---
+++ alias.sh
@@ -0,0 +1,44 @@
+# aliases
+# aliases for (modern) ls
+alias ls="exa"
+alias ll="ls -l"
+alias la="ls -a"
+alias lla="ls -al"
+
+# aliases for (modern) cat
+alias cat="bat"
+alias catp="cat -p"
+
+# aliases for rm/mv/cp
+alias rm="rm -i"
+alias mv="mv -v"
+alias cp="cp -v"
+
+# aliases for git
+alias ga="git add"
+alias gp="git push"
+alias gl="git log"
+alias gs="git status"
+alias gd="git diff"
+alias gm="git commit -m"
+alias gma="git commit -am"
+alias gb="git branch"
+alias gc="git checkout"
+alias gra="git remote add"
+alias grr="git remote rm"
+alias gpu="git pull"
+alias gcl="git clone"
+
+# aliases for homebrew
+alias brewp="brew pin"
+alias brews="brew list -1"
+alias brewsp="brew list --pinned"
+alias bubo="brew update && brew outdated --formula && brew outdated --cask"
+alias bubc='brew upgrade && brew cleanup'
+alias bubu="bubo && bubc"
+alias buf="brew upgrade --formula"
+alias bcubo="brew update && brew outdated --cask"
+alias bcubc="brew upgrade --cask && brew cleanup"
+
+# aliases for python
+alias python="python3"
