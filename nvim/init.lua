38964cefb8b79e4fdf4c5667f9eabc4b48cc49da
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-03-30 23:40:53 -0700

    Add Python LSP with mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,10 @@ require('plugins')
 require('lualine').setup(require('config.lualine'))
 require('nvim-autopairs').setup({})
 require('nvim-treesitter.configs').setup(require('config.treesitter'))
+require('mason').setup()
+
+-- load lsp config
+require'lspconfig'.pylsp.setup{}
 
 -- load neovim colorscheme
 require('onedark').load()
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -6,6 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
 
 	use 'navarasu/onedark.nvim'
 
@@ -21,6 +22,12 @@ return require('packer').startup(function(use)
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
+    }
+
+
+    use {
+        "williamboman/mason.nvim",
+        run = ":MasonUpdate" -- :MasonUpdate updates registry contents
     }
 
 end)
