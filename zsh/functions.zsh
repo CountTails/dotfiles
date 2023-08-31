32700ed13a22f7d3d26fee39c1c1de13e607fd0a
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2023-08-30 23:26:55 -0700

    Add function for discovering md files and creating links

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -11,3 +11,13 @@ function brewupall() {
     # upgrade any other packages (like casks)
     brew upgrade --greedy
 }
+
+# find all markdown files in a given directory and create markdown links from them
+function md_links() {
+    find "$1" -type f -name "*.md" | while read -r file_path; do
+        file_name=$(basename "$file_path" .md)
+        formatted_title=$(echo "$file_name" | awk '{ for(i=1; i<=NF; i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2)); } 1' | sed -E "s/(^|-)\w/\U&/g; s/-/ /g")
+        echo "[$formatted_title]($file_path)"
+    done 
+}
+
